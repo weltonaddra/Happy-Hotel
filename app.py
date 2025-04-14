e76b9ebfd53504726f649d864e0375973e96098d
Welton Addra <addrawork@gmail.com>
2025-04-14 17:45:51 +0000

    App created

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Flask in Codespaces!"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+
+#look for github actions and codebot to check packages
+#Package control
